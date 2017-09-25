242bb509b7925d671a03bca2b17543a92b64fab9
Eric Cornely <ericcornely@gmail.com>
2017-09-25 15:16:20 +0200

    works in junit test but not once deployed...

--- src/test/java/be/nrb/o2/dmn/InMemoryH2Test.java
+++ src/test/java/be/nrb/o2/dmn/InMemoryH2Test.java
@@ -1,19 +1,24 @@
 package be.nrb.o2.dmn;
 
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.processEngine;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.execute;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.job;
 
 import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
@@ -47,12 +52,26 @@ public class InMemoryH2Test {
   @Test
   @Deployment(resources = {"process.bpmn", "DetermineCustomisationWorkflows.dmn"})
   public void testHappyPath() {
-	  ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+    LoggerDelegate loggerDelegate = new LoggerDelegate();
+    Mocks.register("loggerDelegate", loggerDelegate);
+	  ProcessEngine processEngine = processEngine();
+    RuntimeService runtimeService = processEngine.getRuntimeService();
+	  ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+    execute(job(processInstance));
+	  assertThat(processInstance).hasPassed("Task_1l0ld93");
+	  assertThat(processInstance).hasVariables("templateName");
+	  LoggerFactory.getLogger(this.getClass()).debug("The templateName created by javascript is : {}", runtimeService.getVariable(job(processInstance).getExecutionId(), "templateName"));
 	  execute(job(processInstance));
 	  execute(job(processInstance));
+	  assertThat(processInstance).hasPassed("Task_09iy3jw");
+	  assertThat(processInstance).hasVariables("workflowIds");
+	  Object workflowIds = runtimeService.getVariable(job(processInstance).getExecutionId(), "workflowIds");
+    LoggerFactory.getLogger(this.getClass()).debug("The workflowIds created by javascript is : {}", workflowIds);
 	  execute(job(processInstance));
-//	  assertThat(processInstance).isEnded();
-//	  assertThat(processInstance).hasPassed("Task_1lcxg6e");
+	  execute(job(processInstance));
+	  assertThat(processInstance).hasPassed("Task_1lcxg6e");
+	  execute(job(processInstance));
+	  assertThat(processInstance).isEnded();
   }
 
 }
